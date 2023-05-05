62382a3304a44fa1b6c78d647e358d42f44af414
JoakimWibe <71314249+JoakimWibe@users.noreply.github.com>
2023-05-05 14:28:09 +0200

    Update api.ts

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -5,7 +5,7 @@ import { SavedPlan } from '../pages/Plan';
 export const epjcAttractions = 'https://epjcattractions.azurewebsites.net/api/Activities?location=';
 export const travelAdvisorSearchUrl = 'https://travel-advisor.p.rapidapi.com/locations/search';
 export const traveAdvisorAttractionsUrl = 'https://travel-advisor.p.rapidapi.com/attractions/list';
-export const apiKey = '4887eda678mshdc0804b3f912ba8p11db58jsn1475c5f5dc16';
+export const apiKey = '81a0120b97mshb9d5464f3ebd938p1469e7jsn8e97aaa79c40';
 export const apiHost = 'travel-advisor.p.rapidapi.com';
 export const loginUrl = 'https://epjctripapi.azurewebsites.net/login';
 export const registerUrl = 'https://epjctripapi.azurewebsites.net/register';
